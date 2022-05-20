bd28e72ac3fa615507a2609bc0724619a62e4413
Kirill Zubrilin <zubrilinkv@yandex.ru>
2022-05-20 10:24:24 +0300

    Update 19.05

--- KS.py
+++ KS.py
@@ -1,7 +1,10 @@
 from scipy.stats import kstest
 from scipy.stats import norm, skew, kurtosis, skewtest, kurtosistest
+from scipy import stats as st
 import numpy as np
 import matplotlib.pyplot as plt
+
+N = 100_000
 
 files = ['mnu2_mainz.dat', 'mnu2_katrin.dat', 'mnu2_troitsk.dat']
 
@@ -17,35 +20,35 @@ with open(file, 'r') as f:
         i += 1
         data = list(map(float, row.split()))
         mnu2[file].append(data[1])
-        if i == 10000:
+        if i == N:
             break
 
-mu, sigma = 0, 0.19
+mu, sigma = 0, 0.18
 # data = np.random.normal(mu, sigma, size=100_000)
 data = mnu2[file]
-print(kstest(data, "norm", (mu,sigma) ))
-print(skewtest(data))
-print(kurtosistest(data))
+dist = getattr(st, "norm")        
+param = dist.fit(data)
+mu, sigma = param
+print(param)
 
-count, bins_count = np.histogram(data, bins=50)
-  
-# finding the PDF of the histogram using count values
-pdf = count / sum(count)
-  
-# using numpy np.cumsum to calculate the CDF
-# We can also find using the PDF values by looping and adding
+count, bins_count = np.histogram(data, bins=100)
+pdf = count / sum(count)  
 cdf = np.cumsum(pdf)
   
+
+KStest, p = kstest(data, "norm", param )
+print(skewtest(data))
+print(kurtosistest(data))
 # plotting PDF and CDF
 # plt.plot(bins_count[1:], pdf, color="red", label="PDF")
 # plt.plot(bins_count[1:], cdf, "g-o", label="CDF")
-plt.step(bins_count[1:], cdf, "g", where='mid', label="CDF")
+plt.step(bins_count[1:], cdf, "g", where='mid', label="CDF of data")
 
 x = np.linspace(-0.8, 0.8, 100)
-plt.plot(x, norm.cdf(x, mu, sigma), label='normal')
+plt.plot(x, norm.cdf(x, mu, sigma), label=f'normal, \n$\sigma$ = {sigma} \n$\mu$ = {mu}')
 plt.grid()
-plt.title(file)
+plt.title(file + f"  N = {N}")
 
-info = [f"skew = {skew(data):.4f}\n kurt = {kurtosis(data):.4f}"]
+info = [f"skew = {skew(data):.4f}\n kurt = {kurtosis(data, fisher=False):.4f}\n KStest = {KStest:.4f}\n pvalue = {p:.4f}"]
 plt.legend(title='\n'.join(info))
 plt.show()
